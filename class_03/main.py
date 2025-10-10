02d0115fb7e54a96c9a5d6a00b1bf28a29ca32e2
Danii9790 <daniyalashraf9790@gmail.com>
2025-10-10 23:28:13 +0500

    Update

--- class_03/main.py
+++ class_03/main.py
@@ -1,6 +1,6 @@
 import asyncio
-from agents import Agent,Runner,AsyncOpenAI,OpenAIChatCompletionsModel,handoff
-from agents.run import RunConfig
+from agents import Agent,Runner,AsyncOpenAI,OpenAIChatCompletionsModel, function_tool,handoff
+from agents.extensions import handoff_filters
 import os
 from dotenv import load_dotenv
 from agents.agent import StopAtTools
@@ -20,12 +20,24 @@ model = OpenAIChatCompletionsModel(
     model = "gemini-2.0-flash-exp",
     openai_client= external_client
 )
+@function_tool
+def get_weather(city : str) -> str:
+    return f"The weather in {city} is sunny."
 
 refund_agent = Agent(name="Refund Agent")
-general_agent = Agent(name="General Agent",handoffs=[handoff(agent=refund_agent,tool_name_override="Refund order",tool_description_override="Refund the order")])
+general_agent = Agent(name="General Agent",
+tools=[get_weather],
+handoffs=[handoff(agent=refund_agent,tool_name_override="Refund_order",tool_description_override="Refund the order"
+,is_enabled=True
+,input_filter=handoff_filters.remove_all_tools
+
+)])
 
 async def main():
-    result = await Runner.run(general_agent,"I want to refund my order.")
+    result = await Runner.run(general_agent,"""
+    What is the weather in karachi and also
+    I want to refund my order . order nummber is (546890) amount = 2000
+    """)
     print(result.final_output)
     print("Last Agent : ",result.last_agent.name)
 
